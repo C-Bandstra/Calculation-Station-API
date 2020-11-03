c1e172c55b3fadd0053c11ee69fd4fb1a49d9fed
Charlie Bandstra <bandstracharlie@gmail.com>
2020-11-02 17:04:36 -0700

    Add PORT conditional

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
 
+var PORT = process.env.PORT || 5000
+
 app.get("/url", (req, res, next) => {
   res.json(["5x2=10", "5+14=19"]);
  });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(PORT, () => {
+ console.log("Server running on port 5000");
 });
