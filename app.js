7d8a2ef641bb9b424246975a61950ff21754d5b1
Charlie Bandstra <bandstracharlie@gmail.com>
2020-11-02 17:35:14 -0700

    test response change

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var express = require("express");
-var app = express();
+var express = require('express');
+const bodyParser = require('body-parser');
 
-var PORT = process.env.PORT || 5000
+var app = express();
+var PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const calculations = ['2x2=4', '40/4=10', '8+9=17', '10-1=9']
 
 app.get("/", (req, res, next) => {
   res.json(["5x2=10", "5+14=19"]);
+ });
+
+ app.get("/calculations", (req, res, next) => {
+  res.json(calculations);
  });
 
 app.listen(PORT, () => {
