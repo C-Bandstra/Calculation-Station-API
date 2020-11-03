ad7b7b33d881d282279dd5e9851558d28507ba45
Charlie Bandstra <bandstracharlie@gmail.com>
2020-11-02 20:02:16 -0700

    Remove fake data

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const calculations = ['2x2=4', '40/4=10', '8+9=17', '10-1=9']
+const calculations = []
 
 app.get("/", (req, res, next) => {
   res.json(["5x2=10", "5+14=19"]);
