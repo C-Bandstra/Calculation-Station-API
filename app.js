cba688f8936b0efe34bdb256bb5f480ffb3e7026
Charlie Bandstra <bandstracharlie@gmail.com>
2020-11-02 17:25:08 -0700

    change url

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = express();
 
 var PORT = process.env.PORT || 5000
 
-app.get("/url", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.json(["5x2=10", "5+14=19"]);
  });
 
 app.listen(PORT, () => {
- console.log("Server running on port 5000");
+ console.log(`Server running on port ${PORT}`);
 });
