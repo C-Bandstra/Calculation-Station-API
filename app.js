49d18f6fb47e025e260212d31fc6cf3be768a2b7
Charlie Bandstra <bandstracharlie@gmail.com>
2020-11-09 16:11:26 -0700

    Add semi colons

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ var express = require('express');
 const bodyParser = require('body-parser');
 
 var app = express();
-var cors = require('cors')
+var cors = require('cors');
 var PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const calculations = []
+const calculations = [];
 
  app.get("/calculations", (req, res) => {
   res.json(calculations);
  });
 
  app.post("/addCalculation", (req, res) => {
-  const { sequence } = req.body
-  calculations.length === 10 ? calculations.pop() : null
-  calculations.unshift(sequence)
-  res.json({ ok: true, calculations})
+  const { sequence } = req.body;
+  calculations.length === 10 ? calculations.pop() : null;
+  calculations.unshift(sequence);
+  res.json({ ok: true, calculations});
  })
 
 app.listen(PORT, () => {
